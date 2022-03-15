ff046bf6afca4c762d8039ddd88e9b67a43339c8
Tóth Balázs <toth.balazs.mwzx0d@gmail.com>
2022-03-15 12:41:52 +0100

    Added client temporary DbContext tests and test project.

--- MWZX0D_HFT_2021222.Client/Program.cs
+++ MWZX0D_HFT_2021222.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MWZX0D_HFT_2021222.Repository.Database;
+using System;
+using System.Linq;
 
 namespace MWZX0D_HFT_2021222.Client
 {
@@ -6,7 +8,13 @@ namespace MWZX0D_HFT_2021222.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // REPOSITORY DELETE REFERENCE | ONLY IN DEBUG
+            FormulaDbContext ctx = new FormulaDbContext();
+            var teams = ctx.Teams.ToArray();
+            var drivers = ctx.Drivers.ToArray();
+            var engineManufacturers = ctx.EngineManufacturers.ToArray();
+
+            ;
         }
     }
 }
