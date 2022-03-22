a23622c0266e930b138e9f3beb29502863024975
Tóth Balázs <toth.balazs.mwzx0d@gmail.com>
2022-03-22 11:03:35 +0100

    Added custom exceptions in logic

---
+++ MWZX0D_HFT_2021222.Logic/Exceptions/EngineManufacturerNameIsTooShortException.cs
@@ -0,0 +1,14 @@
+﻿using MWZX0D_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWZX0D_HFT_2021222.Logic.Exceptions
+{
+    public class EngineManufacturerNameIsTooShortException : BaseException
+    {
+        public EngineManufacturer EngineManufacturer { get; set; }
+    }
+}
---
+++ MWZX0D_HFT_2021222.Logic/Exceptions/TeamIsNotExistsException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWZX0D_HFT_2021222.Logic.Exceptions
+{
+    public class TeamIsNotExistsException : BaseException
+    {}
+}
