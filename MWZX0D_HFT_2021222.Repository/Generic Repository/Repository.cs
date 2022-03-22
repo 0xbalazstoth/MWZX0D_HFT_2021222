decfc4ca8d9bca8dd47498f03dfed8da650f7d15
Tóth Balázs <toth.balazs.mwzx0d@gmail.com>
2022-03-22 09:33:16 +0100

    Added general repository

---
+++ MWZX0D_HFT_2021222.Repository/Generic Repository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using MWZX0D_HFT_2021222.Repository.Database;
+using MWZX0D_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWZX0D_HFT_2021222.Repository.Generic_Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected FormulaDbContext ctx;
+
+        protected Repository(FormulaDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract T Read(int id);
+
+        public abstract void Update(T item);
+    }
+}
