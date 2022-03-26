78e7e67c36e2e982be13e69b5ea6865f6c68f94e
Balázs Tóth <grandmastar27@gmail.com>
2022-03-26 11:22:03 +0100

    Updated crud methods in logic classes

--- MWZX0D_HFT_2021222.Logic/Classes/DriverLogic.cs
+++ MWZX0D_HFT_2021222.Logic/Classes/DriverLogic.cs
@@ -3,10 +3,7 @@ using MWZX0D_HFT_2021222.Logic.Interfaces;
 using MWZX0D_HFT_2021222.Models;
 using MWZX0D_HFT_2021222.Repository.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MWZX0D_HFT_2021222.Logic.Classes
 {
