c72ed7cae8ad4d4b3f920e4325b2c25a2c16547d
Balázs Tóth <grandmastar27@gmail.com>
2022-03-26 11:25:54 +0100

    Updated DbContext

--- MWZX0D_HFT_2021222.Repository/Database/FormulaDbContext.cs
+++ MWZX0D_HFT_2021222.Repository/Database/FormulaDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MWZX0D_HFT_2021222.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MWZX0D_HFT_2021222.Repository.Database
 {
