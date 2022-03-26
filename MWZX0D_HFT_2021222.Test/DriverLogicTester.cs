465aaf2beba047a41408de7a7d0236d73c625ea6
Balázs Tóth <grandmastar27@gmail.com>
2022-03-26 16:15:07 +0100

    Added logic tester classes

---
+++ MWZX0D_HFT_2021222.Test/DriverLogicTester.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MWZX0D_HFT_2021222.Test
+{
+    [TestFixture]
+    public class DriverLogicTester
+    {
+
+    }
+}
---
+++ MWZX0D_HFT_2021222.Test/TeamLogicTester.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWZX0D_HFT_2021222.Test
+{
+    [TestFixture]
+    public class TeamLogicTester
+    {
+
+    }
+}
