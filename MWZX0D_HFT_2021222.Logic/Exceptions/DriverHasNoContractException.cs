390831605a048de0be232a19fdcf09f0fd08e9cc
Balázs Tóth <grandmastar27@gmail.com>
2022-03-26 11:20:32 +0100

    Updated exceptions

--- MWZX0D_HFT_2021222.Logic/Exceptions/DriverHasNoContractException.cs
+++ MWZX0D_HFT_2021222.Logic/Exceptions/DriverHasNoContractException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MWZX0D_HFT_2021222.Logic.Exceptions
+﻿namespace MWZX0D_HFT_2021222.Logic.Exceptions
 {
     public class DriverHasNoContractException : BaseException
     {}
--- MWZX0D_HFT_2021222.Logic/Exceptions/EngineManufacturerNameIsTooShortException.cs
+++ MWZX0D_HFT_2021222.Logic/Exceptions/EngineManufacturerNameIsTooShortException.cs
@@ -1,9 +1,4 @@
 ﻿using MWZX0D_HFT_2021222.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MWZX0D_HFT_2021222.Logic.Exceptions
 {
