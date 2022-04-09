dc4365ed0107807ff74335d095796ee635b9f7d5
Balázs Tóth <grandmastar27@gmail.com>
2022-04-09 11:03:03 +0200

    Added RestService

--- MWZX0D_HFT_2021222.Client/Program.cs
+++ MWZX0D_HFT_2021222.Client/Program.cs
@@ -1,6 +1,6 @@
-﻿using MWZX0D_HFT_2021222.Logic.Classes;
-using MWZX0D_HFT_2021222.Repository.Database;
-using MWZX0D_HFT_2021222.Repository.ModelRepositories;
+﻿//using MWZX0D_HFT_2021222.Logic.Classes;
+//using MWZX0D_HFT_2021222.Repository.Database;
+//using MWZX0D_HFT_2021222.Repository.ModelRepositories;
 using System;
 using System.Linq;
 
@@ -11,22 +11,23 @@ namespace MWZX0D_HFT_2021222.Client
         static void Main(string[] args)
         {
             // REPOSITORY DELETE REFERENCE | ONLY IN DEBUG
-            FormulaDbContext ctx = new FormulaDbContext();
-            var teams = ctx.Teams.ToArray();
-            var drivers = ctx.Drivers.ToArray();
-            var engineManufacturers = ctx.EngineManufacturers.ToArray();
+            //FormulaDbContext ctx = new FormulaDbContext();
+            //var teams = ctx.Teams.ToArray();
+            //var drivers = ctx.Drivers.ToArray();
+            //var engineManufacturers = ctx.EngineManufacturers.ToArray();
 
-            var driverRepo = new DriverRepository(ctx);
-            var driverLogic = new DriverLogic(driverRepo);
+            //var driverRepo = new DriverRepository(ctx);
+            //var driverLogic = new DriverLogic(driverRepo);
 
-            var teamRepo = new TeamRepository(ctx);
-            var teamLogic = new TeamLogic(teamRepo);
+            //var teamRepo = new TeamRepository(ctx);
+            //var teamLogic = new TeamLogic(teamRepo);
 
             //var q1 = driverLogic.GetDriversPerEngineManufacturer();
             //var q2 = teamLogic.GetEngineManufacturerByPrincipalNameIfContainsSpecificLetter('a');
             //var q3 = driverLogic.GetDriversWhosNumberIsBetweenSpecificRange("ferrari", "mercedes", 5, 45);
             //var q4 = driverLogic.GetAvgDriversAgeByTheSameEngineBasedTeams();
             //var q5 = driverLogic.GetSumPerHeadquarterAtLeastGivenValue(10);
+
         }
     }
 }
