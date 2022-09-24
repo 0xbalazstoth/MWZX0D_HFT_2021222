a17d8c850ecda3c4b76c04d4289bd83f5d9631c8
Tóth Balázs <toth.balazs.mwzx0d@gmail.com>
2022-09-24 13:25:40 +0200

    JSClient GET Added

--- MWZX0D_HFT_2021222.JSClient/wwwroot/index.js
+++ MWZX0D_HFT_2021222.JSClient/wwwroot/index.js
@@ -1,3 +1,47 @@
-﻿fetch('http://localhost:43412/driver')
+﻿let drivers = [];
+
+fetch('http://localhost:43412/driver')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        drivers = y;
+        console.log(drivers);
+
+        display();
+    });
+
+function display() {
+    drivers.forEach(x => {
+        var table = document.getElementById('resultarea');
+        const tr = document.createElement('tr');
+
+        const idTd = document.createElement('td');
+        const nameTd = document.createElement('td');
+        const numberTd = document.createElement('td');
+        const nationalityTd = document.createElement('td');
+        const bornTd = document.createElement('td');
+        const teamIdTd = document.createElement('td');
+
+        const driverId = document.createTextNode(x.driverId);
+        const driverName = document.createTextNode(x.name);
+        const driverNumber = document.createTextNode(x.number);
+        const driverNationality = document.createTextNode(x.nationality);
+        const driverBorn = document.createTextNode(x.born);
+        const driverTeamId = document.createTextNode(x.teamId);
+
+        idTd.appendChild(driverId);
+        nameTd.appendChild(driverName);
+        numberTd.appendChild(driverNumber);
+        nationalityTd.appendChild(driverNationality);
+        bornTd.appendChild(driverBorn);
+        teamIdTd.appendChild(driverTeamId);
+
+        tr.appendChild(idTd);
+        tr.appendChild(nameTd);
+        tr.appendChild(numberTd);
+        tr.appendChild(nationalityTd);
+        tr.appendChild(bornTd);
+        tr.appendChild(teamIdTd);
+
+        table.appendChild(tr);
+    });
+}
